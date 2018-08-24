8da3cc98a1992c8d78232460d47c93006b30f025
hansopschool <42460383+hansopschool@users.noreply.github.com>
2018-08-24 19:38:24 +0300

    Update exercise1-fix.sh

--- vagrant/fixScripts/exercise1-fix.sh
+++ vagrant/fixScripts/exercise1-fix.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 #add fix to exercise1 here
 curl --ipv4 -v http://www.textfiles.com/art/bnbascii.txt
+
+referance:
+https://stackoverflow.com/questions/45554344/curl-7-failed-to-connect-to-port-80-and-443-on-one-domain
+-2-First Check your /etc/hosts file entries, may be the URL which You're requesting, is pointing to your localhost.
+
+If the URL is not listed in your /etc/hosts file, then try to execute following command to understand the flow of Curl Execution for the particular URL:
+
+curl --ipv4 -v "https://example.com/";
+
+
+
+
