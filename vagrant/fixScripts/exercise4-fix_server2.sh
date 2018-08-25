42af5ecd51687355251e2d279dd7ca74848808dc
hansopschool <42460383+hansopschool@users.noreply.github.com>
2018-08-25 14:41:47 +0300

    Update exercise4-fix_server2.sh

--- vagrant/fixScripts/exercise4-fix_server2.sh
+++ vagrant/fixScripts/exercise4-fix_server2.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 #add fix to exercise4-server2 here
+Added the IP and hostname of server1 here:
+sudo nano /etc/hosts
+
+
+ 
+192.168.100.10  server1
+
+# The following lines are desirable for IPv6 capable hosts
+::1 ip6-localhost ip6-loopback
+fe00::0 ip6-localnet
+ff00::0 ip6-mcastprefix
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+ff02::3 ip6-allhosts
+
+
