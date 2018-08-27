f6995c22c2af253334307632c8a8ab6faf3735b0
hansopschool <42460383+hansopschool@users.noreply.github.com>
2018-08-27 19:04:10 +0300

    Update exercise3-fix.sh

--- vagrant/fixScripts/exercise3-fix.sh
+++ vagrant/fixScripts/exercise3-fix.sh
@@ -1,16 +1,3 @@
-#!/bin/bash
-#add fix to exercise3 here
-Step1
-Received the 403 permission denied message
-Step2
-Removed apache2 and reinstalled it
+!/bin/bash
 sudo apt-get remove --purge apache2 apache2-utils
-Step3 
-Reinstalled apache2
 sudo apt-get install apache2
-Result:
-received this:
-It works!
-This is the default web page for this server.
-
-The web server software is running but no content has been added, yet.
